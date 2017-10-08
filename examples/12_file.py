c77b76b9d8596b28b4d0ae32b26a10138f17b268
elle510 <elle0510@gmail.com>
2017-10-09 01:24:22 +0900

    add file with

--- examples/12_file.py
+++ examples/12_file.py
@@ -66,3 +66,14 @@ f = open("새파일.txt", 'r')
 data = f.read()
 print(data)
 f.close()
+
+# with문과 함께 사용하기
+'''
+f = open("foo.txt", 'w')
+f.write("Life is too short, you need python")
+f.close()
+
+# with문을 이용하면 with 블록을 벗어나는 순간 열린 파일 객체 f가 자동으로 close
+with open("foo.txt", "w") as f:
+    f.write("Life is too short, you need python")
+'''
